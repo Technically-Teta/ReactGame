9f041d6dbbca0c9dd34ccb85b48698481cec9bc1
tushort <samquioh@gmail.com>
2023-08-17 16:29:58 -0700

    changed console log note

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     const allSameValue = dice.every((die) => die.value === firstValue);
     if (allHeld && allSameValue) {
       setTenzies(true);
-      console.log("YOU WON !!");
+      console.log(" CONGRATS YOU WON !!");
     }
   }, [dice]);
 
